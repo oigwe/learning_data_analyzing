bf9dbc282d57c2a894c44438ff1614a8d81af58b
ositaigwe@pursuit.org <j0h@example.com>
2020-02-16 19:29:30 -0500

    programming in python: takaways

---
+++ python/Programming in Python: Takeaways.py
@@ -0,0 +1,29 @@
+#by Dataquest Labs, Inc. - All rights reserved © 2020
+#Syntax
+#Displaying the output of a computer program:
+
+print(1 + 2)
+print(5 * 10)
+
+#Ignoring certain lines of code by using code comments:
+
+# print(1 + 2)
+print(5 * 10)
+# This program will only print 50
+
+#Performing arithmetical operations:
+
+1 + 2
+4 - 5
+30 * 1
+20 / 3
+4**3
+(4 * 18)**2 / 10
+
+#Concepts
+#When we give a computer a set of instructions, we say that we're programming it. To program a computer, we need to write the instructions in a special language, which we call a programming language.
+#Python has syntax rules, and each line of instruction has to comply with these rules. For example, print(23 + 7) print(10 - 6) print(12 + 38) doesn't comply with Python's syntax rules and raises a syntax error.
+#The instructions we send to the computer are collectively known as code. Each line of instruction is known as a line of code.
+#When we write code, we program the computer to do something. For this reason, we also call the code we write a computer program, or a program.
+#The code we write serves as input to the computer. The result of executing the code is called output.
+#The sequence of characters that follows the # symbol is called a code comment. We can use code comments to stop the computer executing a line of code or add information about the code we write.
